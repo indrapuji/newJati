f2bc2fdf6ef03b595f1e1714a7bf8a136ddf0c6e
Indra Puji Novirwan <indrapuji@gmail.com>
2020-12-17 02:37:31 +0700

    ganti nama

--- client/src/pages/FormLogin.js
+++ client/src/pages/FormLogin.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import { Button } from "react-bootstrap";
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { Button } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
-import axios from "axios";
-import Swal from "sweetalert2";
-import host from '../hooks/host'
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import host from '../hooks/host';
 
 export default () => {
   // const host = "http://localhost:3001";
@@ -24,32 +24,32 @@ export default () => {
     },
   };
   const [dataForm, setDataForm] = useState({
-    email: '',
-    password: ''
+    nama: '',
+    password: '',
   });
   const onFormChange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
     setDataForm({
       ...dataForm,
-      [name]: value
+      [name]: value,
     });
-  }
-  const onFormSubmit = async(e) => {
+  };
+  const onFormSubmit = async (e) => {
     try {
       e.preventDefault();
       const { data } = await axios({
         method: 'POST',
         url: `${host}/users/login/anggota`,
-        data: dataForm
-      })
+        data: dataForm,
+      });
       localStorage.setItem('token', data.access_token);
       history.push('/');
     } catch (err) {
-      let msg = "";
+      let msg = '';
       if (err.response) {
         if (Array.isArray(err.response.data.msg)) {
-          msg = err.response.data.msg.join("<br>");
+          msg = err.response.data.msg.join('<br>');
         } else {
           msg = err.response.data.msg;
         }
@@ -59,12 +59,12 @@ export default () => {
         msg = err.message;
       }
       Swal.fire({
-        icon: "error",
-        title: "Oops...",
+        icon: 'error',
+        title: 'Oops...',
         html: `${msg}`,
       });
     }
-  }
+  };
   return (
     <motion.div initial="init" animate="in" exit="out" variants={pageTransition}>
       <div className="wrapper fadeInDown ">
@@ -72,16 +72,16 @@ export default () => {
           <div className="fadeIn first">
             <h1>Login</h1>
           </div>
-          <form onSubmit={ onFormSubmit }>
-            <input type="text" className="fadeIn second textInput" name="email" placeholder="email" onChange={ onFormChange } />
-            <input type="password" className="fadeIn third textInput" name="password" placeholder="password" onChange={ onFormChange } />
-            <div style={{ marginTop: "3%", marginBottom: "3%", marginLeft: "7%", marginRight: "7%" }}>
+          <form onSubmit={onFormSubmit}>
+            <input type="text" className="fadeIn second textInput" name="nama" placeholder="nama" onChange={onFormChange} />
+            <input type="password" className="fadeIn third textInput" name="password" placeholder="password" onChange={onFormChange} />
+            <div style={{ marginTop: '3%', marginBottom: '3%', marginLeft: '7%', marginRight: '7%' }}>
               <Button className="fadeIn fourth" block type="submit">
                 Login
               </Button>
             </div>
-            <div style={{ marginTop: "3%", marginBottom: "3%", marginLeft: "7%", marginRight: "7%" }}>
-              <Button className="fadeIn fourth" block onClick={ () => history.push('/') }>
+            <div style={{ marginTop: '3%', marginBottom: '3%', marginLeft: '7%', marginRight: '7%' }}>
+              <Button className="fadeIn fourth" block onClick={() => history.push('/')}>
                 Back to Home
               </Button>
             </div>
