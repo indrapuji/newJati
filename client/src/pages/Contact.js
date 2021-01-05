4e301bdc9e4ce3de8b65be9f43868d5b1281b251
Indra Puji Novirwan <indrapuji@gmail.com>
2021-01-05 10:30:41 +0700

    new email form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,8 +1,9 @@
-import React from "react";
-import Navigation from "../components/Navigation";
-import Footer from "../components/Footer";
-import { Container, Col, Form, Row, Button } from "react-bootstrap";
-import { motion } from "framer-motion";
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+import { Container, Col, Form, Row, Button } from 'react-bootstrap';
+import { motion } from 'framer-motion';
+import emailjs from 'emailjs-com';
 
 export default () => {
   const pageTransition = {
@@ -16,18 +17,31 @@ export default () => {
       opacity: 0,
     },
   };
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_jk7yd8e', 'template_wsowez2', e.target, 'user_m5KIGFfT4P9UmB2N2qOA7').then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  }
   return (
     <motion.div initial="init" animate="in" exit="out" variants={pageTransition}>
       <Navigation />
       <Container>
-        <p style={{ fontWeight: "bold", textAlign: "center", fontSize: 50 }}>Hubungi Kami</p>
-        <Form>
+        <p style={{ fontWeight: 'bold', textAlign: 'center', fontSize: 50 }}>Hubungi Kami</p>
+        <Form onSubmit={sendEmail}>
           <Form.Group as={Row}>
             <Form.Label column sm={2}>
               Nama
             </Form.Label>
             <Col sm={10}>
-              <Form.Control type="text" placeholder="Nama" />
+              <Form.Control type="text" placeholder="Nama" name="nama" />
             </Col>
           </Form.Group>
           <Form.Group as={Row}>
@@ -35,7 +49,7 @@ export default () => {
               Alamat
             </Form.Label>
             <Col sm={10}>
-              <Form.Control as="textarea" rows="3" placeholder="Alamat" />
+              <Form.Control as="textarea" rows="3" placeholder="Alamat" name="alamat" />
             </Col>
           </Form.Group>
           <Form.Group as={Row}>
@@ -43,7 +57,7 @@ export default () => {
               Telpon
             </Form.Label>
             <Col sm={10}>
-              <Form.Control type="text" placeholder="Telpon" />
+              <Form.Control type="text" placeholder="Telpon" name="telepon" />
             </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="formHorizontalEmail">
@@ -51,7 +65,7 @@ export default () => {
               Email
             </Form.Label>
             <Col sm={10}>
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="email" placeholder="Email" name="email" />
             </Col>
           </Form.Group>
 
@@ -64,7 +78,7 @@ export default () => {
         <div style={{ marginBottom: 30 }}>
           <Row>
             <Col>
-              <p style={{ fontWeight: "bold", fontSize: 20, marginBottom: 5 }}>KANTOR PUSAT</p>
+              <p style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 5 }}>KANTOR PUSAT</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>D/A WISMA PERHUTANI</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>JALAN VILLA NO.1 GATOT SOBROTO</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>KAV. 17 – 18 JAKARTA SELATAN</p>
@@ -78,19 +92,19 @@ export default () => {
         <div style={{ marginBottom: 50 }}>
           <Row>
             <Col>
-              <p style={{ fontWeight: "bold", fontSize: 15, marginBottom: 5 }}>KANTOR PERWAKILAN YKP3.JS JAWA BARAT</p>
+              <p style={{ fontWeight: 'bold', fontSize: 15, marginBottom: 5 }}>KANTOR PERWAKILAN YKP3.JS JAWA BARAT</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>D/A KANTOR PERHUTANI</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>JL.SOEKARNO HATTA NO. 628 KM.14 BANDUNG 40292.</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>TLP. 62.22. 7802792 </p>
             </Col>
             <Col>
-              <p style={{ fontWeight: "bold", fontSize: 15, marginBottom: 5 }}>KANTOR PERWAKILAN YKP3.JS JAWA TENGAH</p>
+              <p style={{ fontWeight: 'bold', fontSize: 15, marginBottom: 5 }}>KANTOR PERWAKILAN YKP3.JS JAWA TENGAH</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>D/A KANTOR PERHUTANI</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>JL.PAHLAWAN NO. 15 - 17 SEMARANG 50243</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>TLP. 62.24 8413631</p>
             </Col>
             <Col>
-              <p style={{ fontWeight: "bold", fontSize: 15, marginBottom: 5 }}>KANTOR PERWAKILAN YKP3.JS JAWA TIMUR</p>
+              <p style={{ fontWeight: 'bold', fontSize: 15, marginBottom: 5 }}>KANTOR PERWAKILAN YKP3.JS JAWA TIMUR</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>D/A KANTOR PERHUTANI</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>JL.GENTENG KALI NO. 49 SURABAYA 60008.</p>
               <p style={{ marginBottom: 0, fontSize: 13 }}>TLP. 62.31 5343881</p>
