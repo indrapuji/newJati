a93a3e01517e210276890b4073832adb442ea5f0
Indra Puji Novirwan <indrapuji@gmail.com>
2021-02-07 15:20:28 +0700

    chart simple

--- content/src/views/dashboard/WidgetsDropdown.js
+++ content/src/views/dashboard/WidgetsDropdown.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CWidgetDropdown, CRow, CCol, CDropdown, CDropdownMenu, CDropdownItem, CDropdownToggle } from '@coreui/react';
 import CIcon from '@coreui/icons-react';
-import ChartLineSimple from '../charts/ChartLineSimple';
+import ChartLineSimple from '../../utilities/ChartLineSimple';
 
 const WidgetsDropdown = (props) => {
   const { data } = props;
